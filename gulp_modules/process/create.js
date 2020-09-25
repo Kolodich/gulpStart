b87b388a130d2ce20fe04f3dad62fc8c7cd5fa81
Kolodich <50042392+Kolodich@users.noreply.github.com>
2020-09-25 14:13:42 +0300

    Change file structure and creating new features

--- gulp_modules/process/create.js
+++ gulp_modules/process/create.js
@@ -3,56 +3,92 @@ const { log } = require('gulp-clean/utils');
 const PATH = require("../../settings").PATH;
 
 let fileExtension = [".html", ".scss", ".js"];
+let layoutTypes = {
+  page: "page",
+  block: "block",
+};
 
-function deleteJunks(dirs) {
- dirs.forEach((elem, index) => {
-  if(dirs[index].indexOf(".html")>=0 && dirs[index].indexOf(".js")>=0 && dirs[index].indexOf(".scss")>=0) delete dirs[index]; 
-});
-}
-
-function createFilesByPath(path) {
+function createFilesByPath(path, layoutType) {
  let filePaths = path;
  let dirs = fs.readdirSync(filePaths);
-
  deleteJunks(dirs);
-
  dirs = dirs.filter(function (el) {return el != null;});
  for(i = 0; i < dirs.length;i++){
   let allFilePath = `${filePaths}${dirs[i]}/${dirs[i]}`;
   fileExtension.forEach(elem => {
    if(fs.existsSync(allFilePath+elem) === false){
-    console.log(elem);
     let writedText = ``;
-    if(elem==".html"){
-      
-    }
-    else if(elem==".scss"){
-writedText = 
-`// Global
-@import "../../../global";
+    writedText = writeText(dirs[i], layoutType, elem);
 
-// Mixins
-
-// Code
-.${dirs[i]}{
-
-}`;
-    }
-    else if(elem==".js"){
-
-    }
     fs.appendFileSync(allFilePath+elem, writedText,(err)=>{if(err) throw err;});   
    } 
   });
  } 
 } 
 
+function writeText(dirName, layoutType, fileType) {
+  let text = ``;
+  if(layoutType==layoutTypes.block){
+    if(fileType==".html"){
+      text = `<!-- Start "${dirName}" block -->
+<div class="nav">\n\n</div>
+<!-- End "${dirName}" block -->`;
+    }
+    else if(fileType==".scss"){
+      text = `// Global code
+@import "../../../global.scss";\n
+// "${dirName}" variables \n
+// "${dirName}" mixins \n
+// "${dirName}" code
+.${dirName}{\n\n};`;
+    }
+    else if(fileType==".js"){
+    }      
+  }
+  else if(layoutType=layoutTypes.page){
+    if(fileType==".html"){}
+    else if(fileType==".scss"){
+      text = `// Global code
+@import "../../../global.scss";
+
+// Fonts
+
+// Root, html and body code
+@include root;
+html{}
+body{}
+
+// Common blocks
+@import "../../../modules/common/scss/reset.scss";
+@import "../../../modules/blocks/footer/footer";
+@import "../../../modules/blocks/header/header";
+@import "../../../modules/blocks/main/main";
+@import "../../../modules/blocks/nav/nav";
+@import "../../../modules/blocks/wrapper/wrapper";
+@import "../../../modules/blocks/btn/btn.scss";
+
+// Blocks
+
+// Costom code
+`;
+    }
+    else if(fileType==".js"){}      
+  }
+  return text;
+}
+
+function deleteJunks(dirs) {
+  dirs.forEach((elem, index) => {
+   if(dirs[index].indexOf(".html")>=0 && dirs[index].indexOf(".js")>=0 && dirs[index].indexOf(".scss")>=0) delete dirs[index]; 
+ });
+ }
+
 function createBlocks(){
- createFilesByPath(PATH.src.modulesBlocks);
+ createFilesByPath(PATH.src.modulesBlocks, layoutTypes.block);
 }
 
 function createPages(){
- createFilesByPath(PATH.src.viewsPages);
+ createFilesByPath(PATH.src.viewsPages, layoutTypes.page);
 }
 
 function createBlockAndPages() {
---
+++ src/views/common/js/animItems.js
@@ -0,0 +1,42 @@
+const animItems = document.querySelectorAll(".anim-item");
+if(animItems.length > 0){
+ animScroll(animItems);
+ window.addEventListener("scroll", ()=>{animScroll(animItems)});
+};
+
+function animScroll(animItems) {
+ for (let i = 0; i < animItems.length; i++) {
+  const animItem = animItems[i];
+  const animItemHeight = animItem.offsetHeight;
+  const animItemOffset = offset(animItem).top;
+  const animStart = 4;
+  let animItemPoint = animPoints(animItemHeight, animStart);
+  if((pageYOffset > animItemOffset - animItemPoint) &&  pageYOffset < (animItemOffset + animItemHeight)){
+   animItem.classList.add("_active");
+  }
+  else{
+   if(animItem.classList.contains("repeat-show")){
+    animItem.classList.remove("_active");
+   }
+  };
+ }
+};
+
+function animPoints(animItemHeight, animStart){
+ let animItemPoint = window.innerHeight - animItemHeight / animStart;
+ if(animItemHeight > window.innerHeight){
+  animItemPoint = window.innerHeight - window.innerHeight / animStart;
+ };
+ return animItemPoint;
+}
+
+function offset(el) {
+ const rect = el.getBoundingClientRect();
+ const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+ const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+ const offset = {
+  top: rect.top + scrollTop,
+  left: rect.left + scrollLeft
+ };
+ return offset;
+}
